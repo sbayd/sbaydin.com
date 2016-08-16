8f038293aacf78da782f5a7e12d5c8dfd5565f1a
Berkay Aydın <sbayd06@gmail.com>
2016-08-16 23:35:50 +0300

    projects updated

--- index.php
+++ index.php
@@ -38,6 +38,7 @@
             <img class="profile" src="assets/images/profile.png" alt=""/>
             <h1 class="name">S.Berkay Aydın</h1>
             <h3 class="tagline">Full Stack Developer</h3>
+            <h3 class="tagline"><a href="berkay_aydin_cv.pdf" target="_blank">Download PDF Resume</a></h3>
         </div><!--//profile-container-->
 
         <div class="contact-container container-block">
@@ -54,14 +55,15 @@
                         target="_blank">greadea.com</a></li>
                 <li class="linkedin"><i class="fa fa-linkedin"></i><a href="http://www.linkedin.com/in/sbaydin"
                                                                       target="_blank">linkedin.com/in/sbaydin </a></li>
-                <li class="github"><i class="fa fa-github"></i><a href="www.github.com/sbayd" target="_blank">github.com/sbayd</a></li>
+                <li class="github"><i class="fa fa-github"></i><a href="www.github.com/sbayd" target="_blank">github.com/sbayd</a>
+                </li>
 
             </ul>
         </div><!--//contact-container-->
         <div class="education-container container-block">
             <h2 class="container-block-title">Education</h2>
             <div class="item">
-                <h4 class="degree">BSc in <a href="http://www.ebit.hacettepe.edu.tr" target="_blank" >CEIT</a> </h4>
+                <h4 class="degree">BSc in <a href="http://www.ebit.hacettepe.edu.tr" target="_blank">CEIT</a></h4>
                 <h5 class="meta">Hacettepe University</h5>
                 <div class="time">2012 - Present</div>
             </div><!--//item-->
@@ -95,9 +97,11 @@
             <h2 class="section-title"><i class="fa fa-user"></i>Career Profile</h2>
             <div class="summary">
                 <p>I'm coding something since I was 14. In the beginning I was creating game servers for popular games,
-                after that I started with php and created some basic apps for small-sized companies. Then I decided to
-                train myself. And now, I have successfully finished a good many projects for companies, universities and my government
-                you can see the project details <a href="#projects">below.</a> So, may be we can work together!</p>
+                    after that I started with php and created some basic apps for small-sized companies. Then I decided
+                    to
+                    train myself. And now, I have successfully finished a good many projects for companies, universities
+                    and my government
+                    you can see the project details <a href="#projects">below.</a> So, may be we can work together!</p>
             </div><!--//summary-->
         </section><!--//section-->
 
@@ -111,7 +115,8 @@
                     </div><!--//upper-row-->
                 </div><!--//meta-->
                 <div class="details">
-                    <p>We can work together about creating any kind of management systems, mobile apps, image processing projects. My skills are listed
+                    <p>We can work together about creating any kind of management systems, mobile apps, image processing
+                        projects. My skills are listed
                         <a href="#skills">below.</a></p>
                 </div><!--//details-->
             </div><!--//item-->
@@ -122,12 +127,15 @@
                         <h3 class="job-title">Full-Stack Developer</h3>
                         <div class="time">2012 - Present</div>
                     </div><!--//upper-row-->
-                    <div class="company"><a href="http://ydyazilim.com" target="_blank">YD Software</a>, ANKARA METU Technopolis(ODTU Teknokent)</div>
+                    <div class="company"><a href="http://ydyazilim.com" target="_blank">YD Software</a>, ANKARA METU
+                        Technopolis(ODTU Teknokent)
+                    </div>
                 </div><!--//meta-->
                 <div class="details">
                     <p>I am using ASP.Net MVC Framework with N Tier Architecture to develop management systems,
                         learning management systems & big data projects. And i am developing Android / IOS applications
-                        using Xamarin or NDK. You can see projects that I have completed for YD Software <a href="#projects">below.</a></p>
+                        using Xamarin or NDK. You can see projects that I have completed for YD Software <a
+                            href="#projects">below.</a></p>
                 </div><!--//details-->
             </div><!--//item-->
 
@@ -137,7 +145,9 @@
                         <h3 class="job-title">Intern</h3>
                         <div class="time">2012.06 - 2012.10</div>
                     </div><!--//upper-row-->
-                    <div class="company"><a href="http://ydyazilim.com" target="_blank">YD Software</a>, ANKARA METU Technopolis(ODTU Teknokent)</div>
+                    <div class="company"><a href="http://ydyazilim.com" target="_blank">YD Software</a>, ANKARA METU
+                        Technopolis(ODTU Teknokent)
+                    </div>
                 </div><!--//meta-->
                 <div class="details">
                     <p>I was warming up with and Learning Management System including desktop and web apps.</p>
@@ -149,34 +159,118 @@
         <section class="section projects-section" id="projects">
             <h2 class="section-title"><i class="fa fa-archive"></i>Projects</h2>
             <div class="intro">
-                <p>Projects, i have completed are listed below. To get general information about private project or not-completed projects you can get in touch.</p>
+                <p>Projects, i have completed are listed below. To get general information about private project or
+                    not-finished projects you can get in touch.</p>
             </div><!--//intro-->
             <h5>YD Software</h5>
             <hr>
             <div class="item">
-                <span class="project-title"><a href="#hook">Velocity</a></span> - <span class="project-tagline">A responsive website template designed to help startups promote, market and sell their products.</span>
-
+                <span class="project-title"><a href="http://mavimasa.ankara.bel.tr" target="_blank">Mavi Masa</a></span>
+                - <span
+                    class="project-tagline">
+                    Ankara Metropolitan Municipality - Mavi Masa Project - Mobile Applications (C# / Oracle / Android SDK - Java / iOS SDK/ Web Services)
+                </span>
             </div><!--//item-->
             <div class="item">
-                <span class="project-title"><a
-                        href="http://themes.3rdwavemedia.com/website-templates/responsive-bootstrap-theme-web-development-agencies-devstudio/"
-                        target="_blank">DevStudio</a></span> -
-                <span class="project-tagline">A responsive website template designed to help web developers/designers market their services. </span>
+                <span class="project-title"><a href="http://giris.odtuteknokent.com.tr" target="_blank">TKBS</a></span>
+                - <span
+                    class="project-tagline">
+                     METU Technopolis Information System ( C# / SQL Server / Xamarin / MongoDB / Web Services)
+                </span>
             </div><!--//item-->
             <div class="item">
-                <span class="project-title"><a
-                        href="http://themes.3rdwavemedia.com/website-templates/responsive-bootstrap-theme-for-startups-tempo/"
-                        target="_blank">Tempo</a></span> - <span class="project-tagline">A responsive website template designed to help startups promote their products or services and to attract users &amp; investors</span>
+                <span class="project-title"><a href="http://yfyi2.odtuteknokent.com.tr" target="_blank">YFYİ</a></span>
+                - <span
+                    class="project-tagline">
+                    New Business New Ideas(YFYİ) Entrepreneurship Acceleration Program Management System ( C# / SQL Server / Web Services)
+                </span>
             </div><!--//item-->
             <div class="item">
-                <span class="project-title"><a
-                        href="hhttp://themes.3rdwavemedia.com/website-templates/responsive-bootstrap-theme-mobile-apps-atom/"
-                        target="_blank">Atom</a></span> - <span class="project-tagline">A comprehensive website template solution for startups/developers to market their mobile apps. </span>
+                <span class="project-title"><a href="http://ggj.odtuteknokent.com.tr"
+                                               target="_blank">Global Game Jam</a></span>
+                - <span
+                    class="project-tagline">
+                    Global Game Jam (METU Technopolis) Management System ( C# / SQL Server / Web Services)
+                </span>
             </div><!--//item-->
             <div class="item">
-                <span class="project-title"><a
-                        href="http://themes.3rdwavemedia.com/website-templates/responsive-bootstrap-theme-for-mobile-apps-delta/"
-                        target="_blank">Delta</a></span> - <span class="project-tagline">A responsive Bootstrap one page theme designed to help app developers promote their mobile apps</span>
+                <span class="project-title"><a href="http://atom.odtuteknokent.com.tr" target="_blank">ATOM in Games</a></span>
+                - <span
+                    class="project-tagline">
+                    ATOM Pre-Incubation Center (METU Technopolis) Management System ( C# / SQL Server / Web Services)
+                </span>
+            </div><!--//item-->
+            <div class="item">
+                <span class="project-title"><a href="http://sanayipolitikalari.sanayi.gov.tr" target="_blank">Sanayi Politikalari Portali</a></span>
+                - <span
+                    class="project-tagline">
+                    T.C. Industry Ministry Industrial Policies Portal ( C# / SQL Server / Web Services)
+                </span>
+            </div><!--//item-->
+            <div class="item">
+                <span class="project-title"><a href="http://teknojump2.odtuteknokent.com.tr"
+                                               target="_blank">TeknoJUMP</a></span>
+                - <span
+                    class="project-tagline">
+                    TeknoJUMP Entrepreneurship Acceleration Program Management System ( C# / SQL Server / Web Services)
+                </span>
+            </div><!--//item-->
+            <div class="item">
+                <span class="project-title"><a href="http://bigg.odtuteknokent.com.tr" target="_blank">TUBITAK BIGG / 1512</a></span>
+                - <span
+                    class="project-tagline">
+                    TUBITAK Invidual Young Enterprise(BİGG/1512) Program Management System - METU ( C# / SQL Server / Web Services )
+                </span>
+            </div><!--//item-->
+            <div class="item">
+                <span class="project-title"><a href="http://aosb.org.tr" target="_blank">AOSB</a></span>
+                - <span
+                    class="project-tagline">
+                    Ankara Chamber of Industry 1st Organized Industrial Zone Web Portal - Mobile Applications (C#, SQL Server, Android SDK - Java / iOS SDK / Web Services)
+                </span>
+            </div><!--//item-->
+            <div class="item">
+                <span class="project-title"><a href="#" >AMS App</a></span>
+                - <span
+                    class="project-tagline">
+                    Central Bank of the Republic of Turkey (TCMB) Mobile Archive Management System ( Android SDK / Xamarin / Urovo SDK)
+                </span>
+            </div><!--//item-->
+            <div class="item">
+                <span class="project-title"><a href="http://yee.org.tr" target="_blank">YEE Z-BOOK</a></span>
+                - <span
+                    class="project-tagline">
+                    Yunus Emre Institute Z-Book Education Platform (C#/ MongoDB/ SQLite, Web Services)
+                </span>
+            </div><!--//item-->
+            <div class="item">
+                <span class="project-title"><a href="http://evdekiogretmen.bilfen.com" target="_blank">Evdeki Ögretmen(Teacher at Home)</a></span>
+                - <span
+                    class="project-tagline">
+                Bilfen Evdeki Ogretmen Online Education System (C# / SQL Server / Web Services)
+                </span>
+            </div><!--//item-->
+            <h5>Extracurricular Activities</h5>
+            <div class="item">
+                <span class="project-title"><a href="#" >Mobile Exam System</a></span>
+                - <span
+                    class="project-tagline">
+                A mobile exam project for a company written with Xamarin.Android, Laravel
+                </span>
+            </div><!--//item-->
+            <div class="item">
+                <span class="project-title"><a href="http://www.karmer.hacettepe.edu.tr/hutest/" target="_blank">RIASEC Student Career Profile</a></span>
+                - <span
+                    class="project-tagline">
+                Hacettepe University Career Development Application And Research Center Student Profile Application ( PHP(CI), MySQL, MongoDB)
+                </span>
+            </div><!--//item-->
+            <div class="item">
+                <span class="project-title"><a href="#" >Educational Mobile Game Framework</a></span>
+                - <span
+                    class="project-tagline">
+                    A personal project for physical interactive mobile game development ( C#, Xamarin, Vuforia , OpenCV, MongoDB)
+                </span>
             </div><!--//item-->
         </section><!--//section-->
 
@@ -191,7 +285,7 @@
                     <li>C++</li>
                     <li>OPENCV</li>
                     <li>XAMARIN</li>
-                    <li>ANDROID SDK </li>
+                    <li>ANDROID SDK</li>
                     <li>LARAVEL</li>
                     <li>JAVA</li>
                     <li>SQL SERVER</li>
