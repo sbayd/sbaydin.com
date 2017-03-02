3e76e6e18e93b7e6314d9fb5860398cfc2fa3028
Berkay Aydın <sbayd06@gmail.com>
2017-03-02 19:15:48 +0200

    Update index.php

--- index.php
+++ index.php
@@ -156,6 +156,16 @@
 <script type="text/javascript" src="assets/jsx/babel/skillset.js"></script>
 <script type="text/javascript" src="assets/jsx/babel/experiences.js"></script>
 <script type="text/javascript" src="assets/jsx/babel/main.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-92928189-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </body>
 </html> 
 
